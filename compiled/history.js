56cb4e8326d7d3733748201f683c95cf6605cfe0
Adam C. Baker <adam.durandal@gmail.com>
2014-02-14 11:26:22 -0800

    try testling preprocess to generate compiled

--- compiled/history.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  var stream, hist;
-  stream = require('./streams');
-  hist = require('../bower_components/history/scripts/bundled-uncompressed/html4+html5/native.history');
-}).call(this);

992c67a6c3704d76fb83402e675e597190612cd0
Adam C. Baker <adam.durandal@gmail.com>
2014-02-14 11:18:44 -0800

    check in compiled files for testling.

---
+++ compiled/history.js
@@ -0,0 +1,5 @@
+(function(){
+  var stream, hist;
+  stream = require('./streams');
+  hist = require('../bower_components/history/scripts/bundled-uncompressed/html4+html5/native.history');
+}).call(this);
